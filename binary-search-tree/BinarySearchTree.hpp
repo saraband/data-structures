c8d24f83e3842c02af95e86ec0e6c2c316f212df
Yassine <39649144+saraband@users.noreply.github.com>
2020-07-23 18:19:28 +0200

    wip

--- binary-search-tree/BinarySearchTree.hpp
+++ binary-search-tree/BinarySearchTree.hpp
@@ -12,21 +12,38 @@ class BinarySearchTree
 {
   public:
     BinarySearchTree ()
+      : m_root (nullptr)
     {}
+
+    void insert (int key, T value)
+    {
+
+    }
+
+    // @TODO find, dfs, remove
 
     template <BSTType U, typename V>
     friend std::ostream& operator<< (std::ostream&, const BinarySearchTree<U, V>&);
 
   private:
     struct Node {
-      Node (int k, T v)
+      enum Color {
+        RED,
+        BLACK
+      };
+
+      Node (int k, T v, Color c)
         : key     (k)
         , value   (v)
+        , Color   (c)
       {}
 
-      int   key;
-      T     value;
+      int     key;
+      T       value;
+      Color   color;
     };
+
+    Node* m_root;
 };
 
 template <BSTType type, typename T>
